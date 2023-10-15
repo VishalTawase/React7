77e20cc55f078f2d719849d085620d4162d441d2
VishalTawase <vishalshahuraj.tawase2021@vitstudent.ac.in>
2023-10-15 11:26:58 +0530

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const Calculator = () => {
+  const [expression, setExpression] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(expression));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setExpression('');
+      setResult('');
+    } else {
+      setExpression((prevExpression) => prevExpression + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="output">
+        <input type="text" value={expression} readOnly />
+        <span className="result">{result}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={() => handleButtonClick('=')}>=</button>
+        <button onClick={() => handleButtonClick('C')}>C</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
